5d276b8d82f90f358912069b5049105cc9ccf767
oliverbozhinovski <oliver.bozinovski@wsa.com>
2025-02-10 12:15:15 +0100

    - gradle setup - add data, domain, presentation and di packages - created DawnHealthTestApp.kt

--- app/src/main/java/com/oliver/dawnhealthtestapp/MainActivity.kt
+++ app/src/main/java/com/oliver/dawnhealthtestapp/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.oliver.dawnhealthtestapp.ui.theme.DawnHealthTestAppTheme
+import com.oliver.dawnhealthtestapp.presentation.theme.DawnHealthTestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/oliver/dawnhealthtestapp/presentation/theme/Type.kt
+++ app/src/main/java/com/oliver/dawnhealthtestapp/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.oliver.dawnhealthtestapp.ui.theme
+package com.oliver.dawnhealthtestapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
