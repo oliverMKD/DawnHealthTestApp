e14bd26a23304a7364ad6a29579e6a1b284493d5
oliverbozhinovski <oliver.bozinovski@wsa.com>
2025-02-10 15:59:36 +0100

    - SplashScreen for 3 seconds - Navigation

--- app/src/main/java/com/oliver/dawnhealthtestapp/MainActivity.kt
+++ app/src/main/java/com/oliver/dawnhealthtestapp/MainActivity.kt
@@ -7,41 +7,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.oliver.dawnhealthtestapp.presentation.navigation.AppNavigation
 import com.oliver.dawnhealthtestapp.presentation.theme.DawnHealthTestAppTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        var keepSplash = true
+        installSplashScreen().setKeepOnScreenCondition {
+            keepSplash
+        }
         setContent {
+            LaunchedEffect(Unit) {
+                delay(3000)
+                keepSplash = false
+            }
             DawnHealthTestAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AppNavigation(
+                        activity = this,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DawnHealthTestAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/oliver/dawnhealthtestapp/presentation/SearchScreen.kt
+++ app/src/main/java/com/oliver/dawnhealthtestapp/presentation/SearchScreen.kt
@@ -1,2 +1,12 @@
 package com.oliver.dawnhealthtestapp.presentation
 
+import androidx.compose.runtime.Composable
+import com.oliver.dawnhealthtestapp.presentation.navigation.HomeGraph
+import com.ramcosta.composedestinations.annotation.Destination
+
+@HomeGraph(start = true)
+@Destination
+@Composable
+fun SearchScreen(){
+
+}
